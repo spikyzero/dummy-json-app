d19dd5058eecc66bb29876a6dcc23f6be7c9b4c2
spikyzero <nikeslavko0906@gmail.com>
2024-09-24 18:53:20 +0300

    Implemented base SecurityConfig. Added Thymeleaf.

---
+++ back-end/src/main/java/ua/com/dummy/app/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package ua.com.dummy.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/home").permitAll()
+                        .requestMatchers("/users").authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
--- back-end/src/main/java/ua/com/dummy/app/controller/WebController.java
+++ back-end/src/main/java/ua/com/dummy/app/controller/WebController.java
@@ -4,11 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class WebController {
 
-    @GetMapping
+    @GetMapping("/home")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/users")
+    public String users() {
+        return "users";
+    }
+
+    public String login() {
+        return "login";
     }
 
 }
